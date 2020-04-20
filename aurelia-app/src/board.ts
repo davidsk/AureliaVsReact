a7baeb99c5247e6fe4bc845a29fb5a1c39f21aef
Keith Davidson <keithdavidson@hotmail.com>
2020-04-20 15:56:02 +0100

    Lifting State Up, Again

--- aurelia-app/src/board.ts
+++ aurelia-app/src/board.ts
@@ -1,57 +1,7 @@
-import {computedFrom} from 'aurelia-framework';
-export class Board
-{
-  private squares: Array<any | null>;
-  private xIsNext: Boolean = true;
-  private winner: String | null;
-    
-  @computedFrom('xIsNext', 'winner')
-  get status(): string {
-    return this.winner ? `Winner: ${this.winner}` : `Next player: ${ this.xIsNext ? "X" : "O" }`;
-  }
-  
-  constructor(){
-    this.squares = [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ];
-  }
+import { processContent } from 'aurelia-framework';
 
-  handleClick(rowIndex: number, columnIndex: number){
-    var row = this.squares.slice(rowIndex, rowIndex + 1);
-
-    if (this.winner || row[0][columnIndex]) {
-      return;
-    }    
-    
-    row[0].splice(columnIndex, 1, this.xIsNext ? "X" : "O");
-    this.squares.splice(rowIndex, 1, row[0]);
-    this.xIsNext = !this.xIsNext;
-
-    this.calculateWinner(this.squares);
-  }
-
-  private calculateWinner(squares){
-    const lines = [
-      [[0,0], [0,1], [0,2]],
-      [[1, 0], [1,1], [1,2]],
-      [[2,0], [2,1], [2,2]],
-      [[0,0], [1, 0], [2,0]],
-      [[0,1], [1,1], [2,1]],
-      [[0,2], [1,2], [2,2]],
-      [[0,0], [1,1], [2,2]],
-      [[0,2], [1,1], [2,0]]
-    ];
-    
-    for (let i = 0; i < lines.length; i++) {
-      const [[r1, c1], [r2, c2], [r3,c3]] = lines[i];
-      
-      if (squares[r1][c1] && squares[r1][c1] === squares[r2][c2] && squares[r1][c1] === squares[r3][c3]) {
-        this.winner = squares[r1][c1];
-      }
-    }
-    
-    return this.winner;
-  }
-}
+@processContent((compiler, resources, node, instruction) => {
+   instruction.inheritBindingContext = true;
+   return true;
+ })
+export class Board {}
---
+++ aurelia-app/src/game.ts
@@ -0,0 +1,70 @@
+import {computedFrom} from 'aurelia-framework';
+export class Game
+{
+  private xIsNext: Boolean = true;
+  private winner: String | null;
+  private history;
+  
+  @computedFrom('xIsNext', 'winner')
+  get status(): string {
+    return this.winner ? `Winner: ${this.winner}` : `Next player: ${ this.xIsNext ? "X" : "O" }`;
+  }
+  
+  get current(){
+    return this.history[this.history.length - 1];
+  }
+
+  constructor(){
+    this.history = [
+      {
+        squares : [
+          [null, null, null],
+          [null, null, null],
+          [null, null, null]
+        ]
+      }
+    ]
+  }
+
+  handleClick(rowIndex: number, columnIndex: number){
+    const current = this.history[this.history.length - 1];
+    const squares = current.squares.slice();
+
+    var row = squares.slice(rowIndex, rowIndex + 1);
+
+    if (this.winner || row[0][columnIndex]) {
+      return;
+    }    
+    
+    row[0].splice(columnIndex, 1, this.xIsNext ? "X" : "O");
+    squares.splice(rowIndex, 1, row[0]);
+    
+    this.history.concat(squares);
+    this.xIsNext = !this.xIsNext;
+
+    this.calculateWinner(squares);
+  }
+
+  private calculateWinner(squares){
+    const lines = [
+      [[0,0], [0,1], [0,2]],
+      [[1, 0], [1,1], [1,2]],
+      [[2,0], [2,1], [2,2]],
+      [[0,0], [1, 0], [2,0]],
+      [[0,1], [1,1], [2,1]],
+      [[0,2], [1,2], [2,2]],
+      [[0,0], [1,1], [2,2]],
+      [[0,2], [1,1], [2,0]]
+    ];
+    
+    for (let i = 0; i < lines.length; i++) {
+      const [[r1, c1], [r2, c2], [r3,c3]] = lines[i];
+      
+      if (squares[r1][c1] && squares[r1][c1] === squares[r2][c2] && squares[r1][c1] === squares[r3][c3]) {
+        this.winner = squares[r1][c1];
+      }
+    }
+    
+    return this.winner;
+  }
+}
