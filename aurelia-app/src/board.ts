4918ae0ed5ce338e68464067b0546407a04e07d1
Keith Davidson <keithdavidson@hotmail.com>
2020-04-20 09:23:36 +0100

    Taking Turns

--- aurelia-app/src/board.ts
+++ aurelia-app/src/board.ts
@@ -1,8 +1,13 @@
-
+import {computedFrom} from 'aurelia-framework';
 export class Board
 {
   private squares: Array<any | null>;
-  status: string = "Next player: X";
+  private xIsNext: Boolean = true;
+    
+  @computedFrom('xIsNext')
+  get status(): string {
+    return `Next player: ${ this.xIsNext ? "X" : "O" }`;
+  }
   
   constructor(){
     this.squares = [
@@ -14,7 +19,8 @@ export class Board
 
   handleClick(rowIndex: number, columnIndex: number){
     var row = this.squares.slice(rowIndex, rowIndex + 1);
-    row[0].splice(columnIndex, 1, "X");
+    row[0].splice(columnIndex, 1, this.xIsNext ? "X" : "O");
     this.squares.splice(rowIndex, 1, row[0]);
+    this.xIsNext = !this.xIsNext;
   }
 }
