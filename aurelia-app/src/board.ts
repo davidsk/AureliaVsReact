158a44bcdf9a41ca89f7a7ee6e299499e7343750
Keith Davidson <keithdavidson@hotmail.com>
2020-04-20 10:42:14 +0100

    Declaring a Winner

--- aurelia-app/src/board.ts
+++ aurelia-app/src/board.ts
@@ -3,10 +3,11 @@ export class Board
 {
   private squares: Array<any | null>;
   private xIsNext: Boolean = true;
+  private winner: String | null;
     
-  @computedFrom('xIsNext')
+  @computedFrom('xIsNext', 'winner')
   get status(): string {
-    return `Next player: ${ this.xIsNext ? "X" : "O" }`;
+    return this.winner ? `Winner: ${this.winner}` : `Next player: ${ this.xIsNext ? "X" : "O" }`;
   }
   
   constructor(){
@@ -19,8 +20,38 @@ export class Board
 
   handleClick(rowIndex: number, columnIndex: number){
     var row = this.squares.slice(rowIndex, rowIndex + 1);
+
+    if (this.winner || row[0][columnIndex]) {
+      return;
+    }    
+    
     row[0].splice(columnIndex, 1, this.xIsNext ? "X" : "O");
     this.squares.splice(rowIndex, 1, row[0]);
     this.xIsNext = !this.xIsNext;
+
+    this.calculateWinner(this.squares);
+  }
+
+  private calculateWinner(squares){
+    const lines = [
+      [[0,0], [0,1], [0,2]],
+      [[1, 0], [1,1], [1,2]],
+      [[2,0], [2,1], [2,2]],
+      [[0,0], [1, 0], [2,0]],
+      [[0,1], [1,1], [2,1]],
+      [[0,2], [1,2], [2,2]],
+      [[0,0], [1,1], [2,2]],
+      [[0,2], [1,1], [2,0]]
+    ];
+    
+    for (let i = 0; i < lines.length; i++) {
+      const [[r1, c1], [r2, c2], [r3,c3]] = lines[i];
+      
+      if (squares[r1][c1] && squares[r1][c1] === squares[r2][c2] && squares[r1][c1] === squares[r3][c3]) {
+        this.winner = squares[r1][c1];
+      }
+    }
+    
+    return this.winner;
   }
 }
