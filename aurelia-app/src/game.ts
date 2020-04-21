bb81e3c8658babd00c08b132bda09b516a9dfd8d
Keith Davidson <keithdavidson@hotmail.com>
2020-04-21 12:55:21 +0100

    Implementing Time Travel

--- aurelia-app/src/game.ts
+++ aurelia-app/src/game.ts
@@ -4,6 +4,7 @@ export class Game
   private xIsNext: Boolean = true;
   private winner: String | null;
   private history;
+  stepNumber: number = 0;
 
   @computedFrom('xIsNext', 'winner')
   get status(): string {
@@ -11,7 +12,7 @@ export class Game
   }
   
   get current(){
-    return this.history[this.history.length - 1];
+    return this.history[this.stepNumber];
   }
   
   constructor(){
@@ -27,26 +28,39 @@ export class Game
   }
 
   handleClick(rowIndex: number, columnIndex: number){
+    this.history.splice(this.stepNumber + 1);
     const current = this.history[this.history.length - 1];
-    const squares = current.squares.slice();
 
-    var row = squares.slice(rowIndex, rowIndex + 1);
-
-    if (this.winner || row[0][columnIndex]) {
+    if (this.winner || current.squares[rowIndex][columnIndex]) {
       return;
     }    
     
-    row[0].splice(columnIndex, 1, this.xIsNext ? "X" : "O");
-    squares.splice(rowIndex, 1, row[0]);
-    
-    this.history.push({squares: squares});
-    
-    this.xIsNext = !this.xIsNext;
+    // copy all squares
+    const newSquares = [];
+    for(var rIndex = 0; rIndex < current.squares.length; rIndex++){
+      newSquares[rIndex] = Array(3);
+      for(var cIndex = 0; cIndex < current.squares[rIndex].length; cIndex++){
+        newSquares[rIndex][cIndex] = current.squares[rIndex][cIndex];
+      }
+    }
 
-    this.calculateWinner(squares);
+    newSquares[rowIndex].splice(columnIndex, 1, this.xIsNext ? "X" : "O");
+    
+    this.history.push({squares: newSquares});
+    this.xIsNext = !this.xIsNext;
+    this.stepNumber = this.stepNumber + 1;
+    
+    this.calculateWinner(newSquares, (rowIndex == 0 && columnIndex == 0));
   }
 
-  private calculateWinner(squares){
+  jumpTo(step){
+    this.stepNumber = step;
+    this.xIsNext = (step % 2) === 0;
+
+    this.calculateWinner(this.current.squares);
+  }
+
+  private calculateWinner(squares, log = false){
     const lines = [
       [[0,0], [0,1], [0,2]],
       [[1, 0], [1,1], [1,2]],
@@ -58,8 +72,14 @@ export class Game
       [[0,2], [1,1], [2,0]]
     ];
     
+    this.winner = null;
+
     for (let i = 0; i < lines.length; i++) {
       const [[r1, c1], [r2, c2], [r3,c3]] = lines[i];
+      
+      if(log){
+        console.log(`${squares[r1][c1]}, ${squares[r2][c2]}, ${squares[r3][c3]}`);
+      }
       
       if (squares[r1][c1] && squares[r1][c1] === squares[r2][c2] && squares[r1][c1] === squares[r3][c3]) {
         this.winner = squares[r1][c1];
